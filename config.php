77d54af93de4ea187dcf1260852fb21ea51b32b3
Shpat Kryeziu <shpati.kryeziu@gmail.com>
2024-11-23 12:34:13 +0100

    init

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$username = "root";
+$password = "";
+$host = "localhost";
+$dbname = "gpt_store";
+
+
+try{
+
+    $connect = new PDO("mysql:host=$host;dbname=$database", )
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+}
+?>
